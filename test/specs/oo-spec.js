a2cffd91a150f3db66747b31a8c2e6bf6a76db60
jscriptcoder <jscriptcoder@gmail.com>
2014-02-05 10:22:35 +0100

    added the spec for the library

--- test/specs/oo-spec.js
+++ test/specs/oo-spec.js
@@ -1,3 +1,27 @@
-/**
- * Created by francisco.ramos on 04/02/14.
- */
+describe('Global $JS API', function () {
+
+    it('extends objects [ $JS.extend ]', function () {});
+
+    it('creates namespaces [ $JS.namespace ]', function () {});
+
+    it('defines a module [ $JS.module ]', function () {});
+
+    it('defines an object [ $JS.object ]', function () {});
+
+    it('defines a type [ $JS.type ]', function () {});
+
+    it('defines an interface', function () {});
+
+    describe('Generic definition [ $JS.define ]', function () {
+
+        it('defines a module [ $JS.define("path.to.module", { $module: true, ... }) ]', function () {});
+
+        it('defines an object [ $JS.define("path.to.object", {...}) ]', function () {});
+
+        it('defines a class [ $JS.define("path.to.class", { constructor: function () {...} }) ]', function () {});
+
+        it('defines an interface [ $JS.define("path.to.interface", [...]) ]', function () {});
+
+    });
+
+});
