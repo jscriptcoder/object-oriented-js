35e835c10d42e7772bea949fb7a26a671306eb09
jscriptcoder <jscriptcoder@gmail.com>
2014-02-05 13:45:42 +0100

    added unit test - $JS.extend

--- test/specs/oo-spec.js
+++ test/specs/oo-spec.js
@@ -1,26 +1,68 @@
 describe('Global $JS API', function () {
 
-    it('extends objects [ $JS.extend ]', function () {});
+    it('extends objects [ $JS.extend ]', function () {
 
-    it('creates namespaces [ $JS.namespace ]', function () {});
+        var dst =   { prop1: true },
+            obj1 =  { prop2: 'Jasmine' },
+            obj2 =  { prop3: 'Testing' },
+            other = { prop4: true, prop5: 6 },
+            result;
 
-    it('defines a module [ $JS.module ]', function () {});
+        $JS.extend(dst, obj1, obj2);
 
-    it('defines an object [ $JS.object ]', function () {});
+        expect(dst.prop1).toEqual(true);
+        expect(dst.prop2).toEqual('Jasmine');
+        expect(dst.prop3).toEqual('Testing');
 
-    it('defines a type [ $JS.type ]', function () {});
+        var res = $JS.extend({}, other, obj1, obj2);
 
-    it('defines an interface', function () {});
+        expect(other.prop2).toBeUndefined();
+        expect(other.prop3).toBeUndefined();
+
+        expect(res.prop2).toEqual('Jasmine');
+        expect(res.prop3).toEqual('Testing');
+        expect(res.prop4).toEqual(true);
+        expect(res.prop5).toEqual(6);
+
+    });
+
+    it('creates namespaces [ $JS.namespace ]', function () {
+        //TODO
+    });
+
+    it('defines a module [ $JS.module ]', function () {
+        //TODO
+    });
+
+    it('defines an object [ $JS.object ]', function () {
+        //TODO
+    });
+
+    it('defines a type [ $JS.type ]', function () {
+        //TODO
+    });
+
+    it('defines an interface', function () {
+        //TODO
+    });
 
     describe('Generic definition [ $JS.define ]', function () {
 
-        it('defines a module [ $JS.define("path.to.module", { $module: true, ... }) ]', function () {});
+        it('defines a module [ $JS.define("path.to.module", { $module: true, ... }) ]', function () {
+            //TODO
+        });
 
-        it('defines an object [ $JS.define("path.to.object", {...}) ]', function () {});
+        it('defines an object [ $JS.define("path.to.object", {...}) ]', function () {
+            //TODO
+        });
 
-        it('defines a class [ $JS.define("path.to.class", { constructor: function () {...} }) ]', function () {});
+        it('defines a class [ $JS.define("path.to.class", { constructor: function () {...} }) ]', function () {
+            //TODO
+        });
 
-        it('defines an interface [ $JS.define("path.to.interface", [...]) ]', function () {});
+        it('defines an interface [ $JS.define("path.to.interface", [...]) ]', function () {
+            //TODO
+        });
 
     });
 
