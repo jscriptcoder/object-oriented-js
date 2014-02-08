5e8a9c7861920691434a8f6220525f63f1bc91ab
Francisco Ramos <jscriptcoder@gmail.com>
2014-02-08 11:49:38 +0100

    added $JS.define unit tests

--- test/specs/oo-spec.js
+++ test/specs/oo-spec.js
@@ -310,19 +310,280 @@ describe('Global $JS API', function () {
     describe('Generic definition [ $JS.define ]', function () {
 
         it('defines a module [ $JS.define("path.to.module", { $module: true, ... }) ]', function () {
-            //TODO
+
+            $JS.define('Your.Module', {
+
+                $module: true,
+
+                func1: function() {},
+                func2: function() { return true; }
+            });
+
+            expect(Your.Module.func1).toEqual(jasmine.any(Function));
+            expect(Your.Module.func2()).toBeTruthy();
+
+            $JS.define('Your.Other.Module', function() {
+                var tester = 'Jasmine';
+
+                return {
+
+                    $module: true,
+
+                    getTester: function() { return tester; }
+                };
+            });
+
+            expect(Your.Other.Module.getTester).toEqual(jasmine.any(Function));
+            expect(Your.Other.Module.getTester()).toEqual('Jasmine');
+
+            window.YourRootModule = {};
+
+            $JS.define(YourRootModule, 'Another', {
+
+                $module: true,
+
+                tester: 'Jasmine'
+            });
+
+            expect(YourRootModule.Another.tester).toEqual('Jasmine');
+
+            var SubModule = $JS.define('Your.Other.SubModule', {
+
+                $module: true,
+                $extends: Your.Other.Module,
+
+                helloTester: function () {
+                    return 'Hello ' + SubModule.getTester();
+                }
+
+            });
+
+            expect(Your.Other.SubModule).toBe(SubModule);
+            expect(Your.Other.SubModule.$super).toBe(Your.Other.Module);
+            expect(Your.Other.SubModule.getTester).toEqual(jasmine.any(Function));
+            expect(Your.Other.SubModule.getTester()).toEqual('Jasmine');
+            expect(Your.Other.SubModule.helloTester).toEqual(jasmine.any(Function));
+            expect(Your.Other.SubModule.helloTester()).toEqual('Hello Jasmine');
+
+            var LastModule = Your.Other.SubModule.$extend('Your.Other.LastModule', {
+
+                goodbyeTester: function () {
+                    return 'Goodbye ' + LastModule.getTester();
+                }
+
+            });
+
+            expect(Your.Other.LastModule).toBe(LastModule);
+            expect(Your.Other.LastModule.$super).toBe(Your.Other.SubModule);
+            expect(Your.Other.LastModule.helloTester()).toEqual('Hello Jasmine');
+            expect(Your.Other.LastModule.goodbyeTester()).toEqual('Goodbye Jasmine');
+
         });
 
         it('defines an object [ $JS.define("path.to.object", {...}) ]', function () {
-            //TODO
+
+            $JS.define('Your.Object', {
+                func1: function () {},
+                func2: function () { return true; }
+            });
+
+            expect(Your.Object.$super).toEqual(Object.prototype);
+            expect(Your.Object.func1).toEqual(jasmine.any(Function));
+            expect(Your.Object.func2()).toBeTruthy();
+
+            $JS.define('Your.Other.Object', function () {
+
+                return {
+                    tester: 'Jasmine',
+                    getTester: function () { return this.tester; }
+                };
+
+            });
+
+            expect(Your.Other.Object.$super).toEqual(Object.prototype);
+            expect(Your.Other.Object.getTester).toEqual(jasmine.any(Function));
+            expect(Your.Other.Object.getTester()).toEqual('Jasmine');
+
+            window.YourRootObject = {}
+
+            $JS.define(YourRootObject, 'Another', {
+                tester: 'Jasmine'
+            });
+
+            expect(YourRootObject.Another.tester).toEqual('Jasmine');
+
+            var SubObject = $JS.define('Your.Other.SubObject', {
+
+                $prototype: Your.Other.Object,
+
+                helloTester: function () {
+                    return 'Hello ' + SubObject.getTester.call(this);
+                }
+
+            });
+
+            expect(Your.Other.SubObject).toEqual(SubObject);
+            expect(Your.Other.SubObject.$super).toEqual(Your.Other.Object);
+            expect(Your.Other.SubObject.getTester).toEqual(jasmine.any(Function));
+            expect(Your.Other.SubObject.getTester()).toEqual('Jasmine');
+            expect(Your.Other.SubObject.helloTester).toEqual(jasmine.any(Function));
+            expect(Your.Other.SubObject.helloTester()).toEqual('Hello Jasmine');
+
+            var LastObject = Your.Other.SubObject.$extend('Your.Other.LastObject', {
+
+                init: function (tester) {
+                    this.tester = tester;
+                },
+
+                goodbyeTester: function () {
+                    return 'Goodbye ' + SubObject.getTester.call(this);
+                }
+
+            });
+
+            expect(Your.Other.LastObject).toBe(LastObject);
+            expect(Your.Other.LastObject.$super).toBe(Your.Other.SubObject);
+            expect(Your.Other.LastObject.helloTester()).toEqual('Hello Jasmine');
+            expect(Your.Other.LastObject.goodbyeTester()).toEqual('Goodbye Jasmine');
+
+            var lastObj = Your.Other.LastObject.$new('QUnit');
+
+            expect(lastObj.$super).toBe(Your.Other.SubObject);
+            expect(lastObj.helloTester()).toEqual('Hello QUnit');
+            expect(lastObj.goodbyeTester()).toEqual('Goodbye QUnit');
+
         });
 
         it('defines a class [ $JS.define("path.to.class", { constructor: function () {...} }) ]', function () {
-            //TODO
+
+            $JS.define('Your.FirstClass', {
+
+                $statics: {
+                    tester: 'Jasmine',
+                    sayHello: function () { return 'Hello'; }
+                },
+
+                constructor: function(attr) {
+                    this.attr = attr;
+                },
+
+                getAttr: function () {
+                    return this.attr;
+                }
+
+            });
+
+            expect(Your.FirstClass).toEqual(jasmine.any(Function));
+            expect(Your.FirstClass.tester).toEqual('Jasmine');
+            expect(Your.FirstClass.sayHello()).toEqual('Hello');
+
+            var youtFirst = new Your.FirstClass('Jasmine');
+
+            expect(youtFirst instanceof Object).toBeTruthy();
+            expect(youtFirst instanceof Your.FirstClass).toBeTruthy();
+            expect(youtFirst.getAttr()).toEqual('Jasmine');
+
+            $JS.define('Your.SecondClass', {
+
+                $extends: Your.FirstClass,
+                $statics: {
+                    sayTester: function () {
+                        return Your.SecondClass.tester;
+                    }
+                },
+
+                constructor: function (attr) {
+                    Your.SecondClass.$super.constructor.call(this, attr);
+                },
+
+                getAttr: function () {
+                    var attr = Your.SecondClass.$super.getAttr.call(this);
+                    return attr + ' is cool!!!';
+                }
+
+            });
+
+            expect(Your.SecondClass).toEqual(jasmine.any(Function));
+            expect(Your.SecondClass.sayTester()).toEqual('Jasmine');
+
+            var yourSecond = new Your.SecondClass('Unit Testing');
+
+            expect(yourSecond instanceof Object).toBeTruthy();
+            expect(yourSecond instanceof Your.FirstClass).toBeTruthy();
+            expect(yourSecond instanceof Your.SecondClass).toBeTruthy();
+            expect(yourSecond.getAttr()).toEqual('Unit Testing is cool!!!');
+
+            window.YourRootClasses = {};
+
+            $JS.define(YourRootClasses, 'Another', {
+
+                $class: true,
+
+                sayTester: function () {
+                    return 'Jasmine';
+                }
+            });
+
+            expect(YourRootClasses.Another).toEqual(jasmine.any(Function));
+            expect(YourRootClasses.Another.prototype.sayTester).toEqual(jasmine.any(Function));
+
+            var ThirdClass = Your.SecondClass.$extend('Your.ThirdClass', {
+
+                constructor: function (attr) {
+                    ThirdClass.$super.constructor.call(this, attr);
+                },
+
+                sayTester: function () {
+                    return ThirdClass.tester;
+                }
+
+            });
+
+            var yourThird = new ThirdClass('You');
+
+            expect(yourThird instanceof Object).toBeTruthy();
+            expect(yourThird instanceof Your.FirstClass).toBeTruthy();
+            expect(yourThird instanceof Your.SecondClass).toBeTruthy();
+            expect(yourThird instanceof Your.ThirdClass).toBeTruthy();
+            expect(yourThird.attr).toEqual('You');
+            expect(yourThird.getAttr()).toEqual('You is cool!!!');
+            expect(yourThird.sayTester()).toEqual('Jasmine');
+
         });
 
         it('defines an interface [ $JS.define("path.to.interface", [...]) ]', function () {
-            //TODO
+
+            $JS.define('Your.Interface1', ['func1', 'func2']);
+
+            expect(Your.Interface1.func1).toEqual(jasmine.any(Function));
+            expect(Your.Interface1.func2).toEqual(jasmine.any(Function));
+
+            $JS.define('Your.Interface2', ['func3', 'func4']);
+
+            expect(Your.Interface2.func3).toThrow();
+            expect(Your.Interface2.func4).toThrow();
+
+            $JS.define('Your.SubInterface1', {
+                $extends: Your.Interface1,
+                $interface: ['func5', 'func6']
+            });
+
+            expect(Your.SubInterface1.func1).toEqual(jasmine.any(Function));
+            expect(Your.SubInterface1.func2).toEqual(jasmine.any(Function));
+            expect(Your.SubInterface1.func5).toEqual(jasmine.any(Function));
+            expect(Your.SubInterface1.func6).toEqual(jasmine.any(Function));
+            expect(Your.SubInterface1.func5).toThrow();
+            expect(Your.SubInterface1.func6).toThrow();
+
+            $JS.define('Your.SubInterface2', {
+                $extends: [Your.Interface1, Your.Interface2],
+                $interface: ['func7', 'func8']
+            });
+
+            expect(Your.SubInterface2.func1).toEqual(jasmine.any(Function));
+            expect(Your.SubInterface2.func3).toEqual(jasmine.any(Function));
+            expect(Your.SubInterface2.func7).toThrow();
+
         });
 
     });
